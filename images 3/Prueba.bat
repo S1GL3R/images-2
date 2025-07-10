7af5b1aa2573b6e629ddfd9a243ca5a1740d9455
Victor Romero <74642040+S1GL3R@users.noreply.github.com>
2025-07-10 08:17:34 -0600

    Add files via upload

---
+++ images 3/Prueba.bat
@@ -0,0 +1,19 @@
+@echo off
+color 4f
+title ERROR CRITICO
+cls
+echo [!] SU SISTEMA ESTA SIENDO HACKEADO...
+timeout /t 2 >nul
+echo [!] ROBANDO DATOS PERSONALES...
+timeout /t 1 >nul
+echo [!] ERROR: DETECCION DE AMENAZA NIVEL 4
+timeout /t 2 >nul
+echo [!] ENVIANDO INFORMACION AL SERVIDOR...
+timeout /t 2 >nul
+echo [!] HA SIDO INFECTADO!!
+timeout /t 3 >nul
+
+start https://youtu.be/dQw4w9WgXcQ?si=yksmQ9rCSMCXHU2S
+
+echo Es broma, no te asustes :D
+pause
